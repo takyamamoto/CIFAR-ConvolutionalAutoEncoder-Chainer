3ffd2c8ee4ef8e79038b925df96564ec06ece71e
takyamamoto <41336057+takyamamoto@users.noreply.github.com>
2018-07-22 14:09:46 +0900

    Fix

--- network.py
+++ network.py
@@ -18,12 +18,12 @@ class CAE(chainer.Chain):
 
     def __init__(self, n_input, n_out, directory=None, return_out=False):
         super(CAE, self).__init__(
-                conv1 = L.Convolution2D(None, 64, 3, pad=1),
-                conv2 = L.Convolution2D(None, 128, 3, pad=1),
-                conv3 = L.Convolution2D(None, 256, 3, pad=1),
-                
-                conv4 = L.Convolution2D(None, 256, 3, pad=1),
-                conv5 = L.Convolution2D(None, 128, 3, pad=1),
+                conv1 = L.Convolution2D(None, 32, 3, pad=1),
+                conv2 = L.Convolution2D(None, 64, 3, pad=1),
+                conv3 = L.Convolution2D(None, 128, 3, pad=1),
+
+                conv4 = L.Convolution2D(None, 128, 3, pad=1),
+                conv5 = L.Convolution2D(None, 64, 3, pad=1),
                 conv6 = L.Convolution2D(None, 3, 3, pad=1)
                 )
         self.directory = directory
@@ -32,21 +32,21 @@ class CAE(chainer.Chain):
     def save_image(self, arr, filename):
         img = chainer.cuda.to_cpu(arr)
         img = img * 255
-        img = img.astype(np.uint8) 
+        img = img.astype(np.uint8)
         img = Image.fromarray(img)
         img.save(filename)
-        
-    def __call__(self, x, t):            
+
+    def __call__(self, x, t):
         # Encoder
         e = F.relu(self.conv1(x))
         e = F.max_pooling_2d(e,ksize=2, stride=2,)
         e = F.relu(self.conv2(e))
         e = F.max_pooling_2d(e,ksize=2, stride=2,)
         e_out = F.relu(self.conv3(e))
-        
+
         # Decoder
         d = F.relu(self.conv4(e_out))
-        d = F.unpooling_2d(e_out, ksize=2, stride=2, cover_all=False)
+        d = F.unpooling_2d(d, ksize=2, stride=2, cover_all=False)
         d = F.relu(self.conv5(d))
         d = F.unpooling_2d(d, ksize=2, stride=2, cover_all=False)
         out = F.sigmoid(self.conv6(d))
@@ -61,14 +61,13 @@ class CAE(chainer.Chain):
                 outi = out[i]
                 outi = F.transpose(outi, (1, 2, 0))
                 self.save_image(outi.data, filename)
-        """         
+        """
 
         loss = F.mean_squared_error(out,t)
-        
+
         reporter.report({'loss': loss}, self)
-        
+
         if self.return_out == True:
             return out, loss
         else:
             return loss
-        
