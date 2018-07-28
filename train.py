24222a1deb3c2b5fae56ed2629b6efe33f7f4d38
takyamamoto <41336057+takyamamoto@users.noreply.github.com>
2018-07-28 21:38:00 +0900

    Add

--- train.py
+++ train.py
@@ -7,15 +7,12 @@ Created on Sat Jul 21 08:51:18 2018
 
 import argparse
 
-import numpy as np
-
 from chainer import training
 from chainer.training import extensions
 from chainer import iterators, optimizers, serializers
 from chainer import cuda
 from chainer.datasets import get_cifar10
 from chainer import dataset
-from chainer import Variable
 
 import network
 
@@ -34,9 +31,10 @@ def main():
     parser.add_argument('--gpu', '-g', type=int, default=-1)
     parser.add_argument('--model', '-m', type=str, default=None)
     parser.add_argument('--opt', type=str, default=None)
-    parser.add_argument('--epoch', '-e', type=int, default=10)
+    parser.add_argument('--epoch', '-e', type=int, default=20)
     parser.add_argument('--lr', '-l', type=float, default=0.001)
     parser.add_argument('--batch', '-b', type=int, default=32)
+    parser.add_argument('--noplot', dest='plot', action='store_false')
     args = parser.parse_args()
 
     # Set up a neural network to train.
@@ -47,9 +45,11 @@ def main():
 
     train_iter = iterators.SerialIterator(train, batch_size=args.batch, shuffle=True)
     test_iter = iterators.SerialIterator(test, batch_size=args.batch, repeat=False, shuffle=False)
-
+    
+    # Define model
     model = network.CAE(3,3)
-
+    
+    # Load weight
     if args.model != None:
         print( "loading model from " + args.model )
         serializers.load_npz(args.model, model)
@@ -58,6 +58,7 @@ def main():
         cuda.get_device_from_id(0).use()
         model.to_gpu()
 
+    # Define optimizer
     opt = optimizers.Adam(alpha=args.lr)
     opt.setup(model)
 
@@ -65,11 +66,18 @@ def main():
         print( "loading opt from " + args.opt )
         serializers.load_npz(args.opt, opt)
 
+    # Set up a trainer
     updater = training.StandardUpdater(train_iter, opt, device=args.gpu)
     trainer = training.Trainer(updater, (args.epoch, 'epoch'), out='results')
 
     trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
     trainer.extend(extensions.LogReport(trigger=(10, 'iteration')))
+    
+    if args.plot and extensions.PlotReport.available():
+        trainer.extend(
+            extensions.PlotReport(['main/loss', 'validation/main/loss'],
+                                  'epoch', file_name='loss.png'))
+    
     trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'validation/main/loss']))
     trainer.extend(extensions.ProgressBar(update_interval=1))
     
